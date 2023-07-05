bbf11003984d232f312eae75813cedeb33c40507
Serpensin <wissmann.matti@gmail.com>
2023-07-05 16:12:45 +0200

    Projektdateien hinzufügen.

---
+++ Killswitch/Killswitch.py
@@ -0,0 +1,73 @@
+import aiohttp
+import html2text
+from bs4 import BeautifulSoup
+
+
+
+async def get_killswitch(return_type='html'):
+    """
+    Gets the current Kill Switch status from the official Dead by Daylight website.
+    
+    Parameters
+    ----------
+    return_type : str, optional
+        The format to return the data in. Can be either 'html' or 'md'. Defaults to 'html'.
+    
+    Returns
+    -------
+    str
+        The Kill Switch status in the requested format.
+    
+    Raises
+    ------
+    ValueError
+        If the return type is not 'html' or 'md'.
+
+    Notes
+    -----
+    The content is retrieved from https://forums.bhvr.com/dead-by-daylight/kb/articles/299-kill-switch-master-list.
+    """
+    if return_type not in ['html', 'md']:
+        raise ValueError("Invalid return type. Return type needs to be either 'html' or 'md'.")
+
+    url = 'https://forums.bhvr.com/dead-by-daylight/kb/articles/299-kill-switch-master-list'
+    converter = html2text.HTML2Text()
+    converter.ignore_links = True
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            page_content = await response.text()
+            soup = BeautifulSoup(page_content, 'html.parser')
+
+            # Find the required sections
+            kill_switch_section = soup.find('h2', {'data-id': 'kill-switch-(disabled)'})
+            known_issues_section = soup.find('h2', {'data-id': 'known-issues-(not-disabled)'})
+
+            # Extract the content between the two sections
+            content = []
+            current_section = kill_switch_section.find_next_sibling()
+            while current_section and current_section != known_issues_section:
+                # Remove any images
+                for img in current_section.find_all('img'):
+                    img.decompose()
+                content.append(str(current_section))
+                current_section = current_section.find_next_sibling()
+
+            # Convert the content to the required format
+            content = '\n'.join(content)
+            if not content:
+                return None
+            elif return_type == 'html':
+                return content
+            elif return_type == 'md':
+                return converter.handle(content)
+
+    return None
+
+
+
+
+if __name__ == '__main__':
+    import asyncio
+    md = asyncio.run(get_killswitch('md'))
+    print(md)
---
+++ Libretrans/Libretrans.py
@@ -0,0 +1,120 @@
+import aiohttp
+
+
+
+
+class LibreTranslateAPI:
+    """
+    A wrapper for the LibreTranslate API.
+
+    Parameters
+    ----------
+    APIkey : str
+        The API key to use for the API.
+    url : str
+        The URL of the API.
+    """
+    def __init__(self, APIkey, url):
+        self.APIkey = APIkey
+        self.url = url
+
+
+    async def _get_sample(self, text):
+        sentences = text.split('. ')
+        first_sentence_words = sentences[0].split(' ')
+
+        if len(first_sentence_words) > 10:
+            sample = ' '.join(first_sentence_words[:10])
+        else:
+            sample = ' '.join(first_sentence_words)
+
+        return sample
+
+
+    async def detect(self, text):
+        """
+        Detects the language of the given text.
+        
+        Parameters
+        ----------
+        text : str
+            The text to detect the language of.
+        
+        Returns
+        -------
+        dict
+            A dictionary containing the status code and the data.
+        200: The request was successful.
+        400: The request was invalid.
+        500: The server encountered an error.
+        """
+        url = f"{self.url}/detect"
+        params = {
+            "q": text,
+            "api_key": self.APIkey
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, params=params) as response:
+                data = await response.json()
+                return {"status": response.status, "data": data}
+
+
+    async def translate(self, text, dest_lang, source = ''):
+        """
+        Translates the given text to the given language.
+
+        Parameters
+        ----------
+        text : str
+            The text to translate.
+        dest_lang : str
+            The language to translate the text to.
+        source : str, optional
+            The language of the text. If not given, it will be detected automatically.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the status code and the data.
+        200: The request was successful.
+        400: The request was invalid.
+        500: The server encountered an error.
+        """
+        url = f'{self.url}/translate'
+        if source == '':
+            source = await self.detect(await self._get_sample(text))
+            source = source["data"][0]["language"]
+        params = {
+            "q": text,
+            "source": source,
+            "target": dest_lang,
+            "api_key": self.APIkey
+        }
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, params=params) as response:
+                    data = await response.json()
+                    return {"status": response.status, "data": data}
+        except:
+            return {"status": 500, "data": None}
+
+
+    async def check_status(self):
+        """
+        Checks the status of the API.
+        
+        Returns
+        -------
+        bool
+            Whether the API is online or not.
+        """
+        url = f'{self.url}/frontend/settings'
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    return response.status == 200
+        except:
+            return False
+
+
+
