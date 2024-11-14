aa8dda5f6bbf52402f60b17dd2fb2095e97ebf46
Serpensin <wissmann.matti@gmail.com>
2024-11-15 00:42:07 +0100

    Refactor invite caching and processing logic
    - Added `import asyncio` to `InviteTracker.py` for async features.
    - Changed `class Tracker():` to `class Tracker:` for convention.
    - Refactored `cache_invites` to use `asyncio.gather` and moved single guild logic to `_cache_guild_invites`.
    - Introduced `_cache_guild_invites` for single guild invite caching.
    - Simplified `update_invite_cache` with `setdefault`.
    - Refactored `add_guild_cache` using dictionary comprehension.
    - Refactored `process_invite` using dictionary comprehension and simplified invite cache update logic.

--- InviteTracker/InviteTracker.py
+++ InviteTracker/InviteTracker.py
@@ -4,8 +4,7 @@ from discord import AuditLogAction
 from discord.errors import Forbidden
 
 
-
-class Tracker():
+class Tracker:
     """
     A class for tracking invites and managing their cache.
     """
@@ -25,13 +24,17 @@ class Tracker():
         Cache invites for all guilds the bot is currently in.
         """
         async with self._lock:
-            for guild in self.bot.guilds:
-                try:
-                    self._cache[guild.id] = {}
-                    for invite in await guild.invites():
-                        self._cache[guild.id][invite.code] = invite
-                except Forbidden:
-                    continue
+            tasks = [self._cache_guild_invites(guild) for guild in self.bot.guilds]
+            await asyncio.gather(*tasks)
+
+    async def _cache_guild_invites(self, guild):
+        """
+        Helper method to cache invites for a single guild.
+        """
+        try:
+            self._cache[guild.id] = {invite.code: invite for invite in await guild.invites()}
+        except Forbidden:
+            pass
 
     async def update_invite_cache(self, invite):
         """
@@ -41,10 +44,7 @@ class Tracker():
             invite (discord.Invite): The invite to update the cache with.
         """
         async with self._lock:
-            guild_id = invite.guild.id
-            if guild_id not in self._cache:
-                self._cache[guild_id] = {}
-            self._cache[guild_id][invite.code] = invite
+            self._cache.setdefault(invite.guild.id, {})[invite.code] = invite
 
     async def remove_invite_cache(self, invite):
         """
@@ -84,9 +84,7 @@ class Tracker():
             guild (discord.Guild): The guild to add invites from to the cache.
         """
         async with self._lock:
-            self._cache[guild.id] = {}
-            for invite in await guild.invites():
-                self._cache[guild.id][invite.code] = invite
+            self._cache[guild.id] = {invite.code: invite for invite in await guild.invites()}
 
     async def remove_guild_cache(self, guild):
         """
@@ -111,18 +109,18 @@ class Tracker():
         await asyncio.sleep(self.bot.latency)
         async with self._lock:
             guild_id = member.guild.id
-            for new_invite in await member.guild.invites():
-                for cached_invite in self._cache[guild_id].values():
-                    if (new_invite.code == cached_invite.code and new_invite.uses - cached_invite.uses == 1) or cached_invite.revoked:
-                        if cached_invite.revoked:
-                            self._cache[guild_id].pop(cached_invite.code, None)
-                        elif new_invite.inviter == cached_invite.inviter:
-                            self._cache[guild_id][cached_invite.code] = new_invite
-                        else:
-                            self._cache[guild_id][cached_invite.code].uses += 1
-                        inviter_id = cached_invite.inviter.id
-                        if inviter_id is not None:
-                            inviter = member.guild.get_member(inviter_id)
-                            return inviter
-                        else:
-                            return None  # Handle the case when inviter_id is None (vanity link)
+            new_invites = {invite.code: invite for invite in await member.guild.invites()}
+            for code, new_invite in new_invites.items():
+                cached_invite = self._cache[guild_id].get(code)
+                if cached_invite and (new_invite.uses - cached_invite.uses == 1 or cached_invite.revoked):
+                    if cached_invite.revoked:
+                        self._cache[guild_id].pop(code, None)
+                    elif new_invite.inviter == cached_invite.inviter:
+                        self._cache[guild_id][code] = new_invite
+                    else:
+                        self._cache[guild_id][code].uses += 1
+                    inviter_id = cached_invite.inviter.id
+                    if inviter_id is not None:
+                        return member.guild.get_member(inviter_id)
+                    else:
+                        return None  # Handle the case when inviter_id is None (vanity link)
